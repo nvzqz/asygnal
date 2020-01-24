5b5d566fe40382e566166031955e0398b246522a
Nikolai Vazquez <nikvzqz@gmail.com>
2020-01-24 00:25:00 -0500

    Escape first "[" for "[WIP]" in docs
    Otherwise, it tries resolving to a reference link.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! [WIP] Async-first signal handling, such as `CTRL` + `C`.
+//! \[WIP] Async-first signal handling, such as `CTRL` + `C`.
 //!
 //! Because this library is async-first, it should be more efficient than using
 //! an alternative approach that awaits a blocking signal handler.
